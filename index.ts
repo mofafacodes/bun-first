927b6039df45d67798a72b064bdfc7a4fefc663c
Nutifafa Kpodo <nutifafakpodo@outlook.com>
2023-10-15 16:39:42 +0000

    adding routes

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
 import figlet from "figlet";
 
-
-
 const server = Bun.serve({
     port : 3000,
     fetch(req){
+        const url = new URL(req.url);
 
-        const body = figlet.textSync("Here we go guys, we are here to learn bun!");
+        if (url.pathname === "/"){
+            const body = figlet.textSync("Here we go guys, we are here to learn bun!");
+            return new Response(body);
+        }
+
+        if (url.pathname === "/about"){
+            const body = figlet.textSync("About bun");
+            return new Response(body);
+        }
+
+        if (url.pathname === "/contact"){
+            const body = figlet.textSync("Contact bun");
         return new Response(body);
+        }
+
+        const body = figlet.textSync("404!");
+        return new Response(body);
+       
     }
 });
 
